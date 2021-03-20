ccd956ba02aafe3155cadb997dbfeeff5c4dfdba
ZERO301318 <13588062753@163.com>
2021-03-21 00:02:15 +0800

    test github

--- app/src/main/java/com/example/calculate/MasterFragment.java
+++ app/src/main/java/com/example/calculate/MasterFragment.java
@@ -16,6 +16,7 @@ public class MasterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.master_fragment, container, false);
     }
 
