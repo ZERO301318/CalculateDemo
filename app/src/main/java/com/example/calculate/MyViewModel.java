94260c20fb667fbec3ba27d81608313d632684c8
ZERO301318 <13588062753@163.com>
2021-03-20 23:52:12 +0800

    test github

---
+++ app/src/main/java/com/example/calculate/MyViewModel.java
@@ -0,0 +1,7 @@
+package com.example.calculate;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/calculate/WinFragment.java
@@ -0,0 +1,19 @@
+package com.example.calculate;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WinFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_win, container, false);
+    }
+}
